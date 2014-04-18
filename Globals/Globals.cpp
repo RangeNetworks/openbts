f508b00a6fc0c80ec0ac41f6be74cc02da90575b
Michael Iedema <michael.iedema@rangenetworks.com>
2014-04-18 11:42:30 +0200

    - reference TIMESTAMP_ISO instead of __DATE__

--- Globals/Globals.cpp
+++ Globals/Globals.cpp
@@ -27,7 +27,7 @@
 
 #define FEATURES "+GPRS "
 
-const char *gVersionString = "release " VERSION FEATURES PROD_CAT " built " __DATE__ " rev" SVN_REV " ";
+const char *gVersionString = "release " VERSION FEATURES PROD_CAT " built " TIMESTAMP_ISO " rev" SVN_REV " ";
 
 const char* gOpenBTSWelcome =
 	//23456789123456789223456789323456789423456789523456789623456789723456789
@@ -35,7 +35,7 @@ const char* gOpenBTSWelcome =
 	"Copyright 2008, 2009, 2010 Free Software Foundation, Inc.\n"
 	"Copyright 2010 Kestrel Signal Processing, Inc.\n"
 	"Copyright 2011, 2012, 2013, 2014 Range Networks, Inc.\n"
-	"Release " VERSION " " PROD_CAT " formal build date " __DATE__ " rev" SVN_REV "\n"
+	"Release " VERSION " " PROD_CAT " formal build date " TIMESTAMP_ISO " rev" SVN_REV "\n"
 	"\"OpenBTS\" is a registered trademark of Range Networks, Inc.\n"
 	"\nContributors:\n"
 	"  Range Networks, Inc.:\n"
