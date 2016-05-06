e09152ccb589c000dca5c9b7ac27e746d764f12d
Michael Iedema <michael.iedema@rangenetworks.com>
2016-05-06 18:19:19 +0200

    update other bits in OpenBTS to use safer read() signature

--- Peering/Peering.cpp
+++ Peering/Peering.cpp
@@ -218,7 +218,7 @@ void* PeerInterface::serviceLoop2(void*)
 
 void PeerInterface::drive()
 {
-	int numRead = mSocket.read(mReadBuffer);
+	int numRead = mSocket.read(mReadBuffer, sizeof(mReadBuffer));
 	if (numRead<0) {
 		return;
 	}
